7b3c2d8cf45541a8f376d8b9180edc2e0c14d8fa
steffandb <steffandb@gmail.com>
2025-06-08 15:07:22 -0300

    Initial structure

---
+++ Encoder.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace RunLengthEncodingApp
+{
+    public static class Encoder
+    {
+        public static string RunLengthEncode(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "";
+
+            StringBuilder result = new StringBuilder();
+            int count = 1;
+            char current = input[0];
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] == current)
+                {
+                    count++;
+                }
+                else
+                {
+                    result.Append(count);
+                    result.Append(current);
+                    current = input[i];
+                    count = 1;
+                }
+            }
+
+            result.Append(count);
+            result.Append(current);
+
+            return result.ToString();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# run-length-encoding
+# Run Length Encoding in C#
+
+This project implements a simple Run-Length Encoding (RLE) library for compressing strings by counting consecutive characters.
+
+## Example
+
+**Input:** `aaabbcdddddddd`  
+**Output:** `3a2b1c8d`
+
+## How to Run
+
+```bash
+dotnet run --project RunLengthEncodingApp
